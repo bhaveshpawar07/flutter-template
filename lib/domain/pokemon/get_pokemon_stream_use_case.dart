39798e3a9367a9dee83c8fc4ca76d3adfc739c73
bhaveshpawar07 <bhavesh.pawar@wednesday.is>
2022-07-06 11:49:59 +0530

    converted to package import.

--- lib/domain/pokemon/get_pokemon_stream_use_case.dart
+++ lib/domain/pokemon/get_pokemon_stream_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_template/domain/base/usecase/base_async_use_case.dart';
-import '../../presentation/entity/pokemon/ui_pokemon.dart';
+import 'package:flutter_template/presentation/entity/pokemon/ui_pokemon.dart';
 
 abstract class GetPokemonStreamUseCase
     extends BaseAsyncUseCase<String, UIPokemon?> {}
